159344de6bdff543c56e2d4d3933f55531d4875e
ElonTusk404 <igmaflappy@gmail.com>
2023-07-10 19:14:06 +0400

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,13 +1,19 @@
+class TargetNotFoundException(Exception):
+    pass
 def binary_search(nums, target):
     if not isinstance(nums, list):
         raise TypeError('Only list')
-    l, r = 0, len(nums) - 1
-    while l <= r:
-        mid = (l + r) // 2
-        if nums[mid] == target:
-            return mid
-        if nums[mid] < target:
-            l = mid + 1
-        else:
-            r = mid - 1
+    try:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+    except Exception:
+        raise TargetNotFoundException('Target not found in the list')
     return -1
+    
