21128a12f49af375df89df3cdb4488d19f838677
ElonTusk404 <igmaflappy@gmail.com>
2023-07-10 19:44:02 +0400

    Update sum_digits.py

--- sum_digits.py
+++ sum_digits.py
@@ -1,6 +1,8 @@
 def sum_digits(num: int):
+    if not isinstance(num, int):
+        raise TypeError('Argument must be only integer')
     if num == 0:
         return num
     
     return num % 10 + sum_digits(num // 10)
-print(sum_digits(155))
+print(sum_digits('omg'))
