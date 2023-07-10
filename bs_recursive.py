a5981a793b5d0e6aa0e85fbba72be1d6a5b6666f
ElonTusk404 <igmaflappy@gmail.com>
2023-07-10 19:30:20 +0400

    Update bs_recursive.py

--- bs_recursive.py
+++ bs_recursive.py
@@ -1,4 +1,11 @@
+class TargetNotFoundException(Exception):
+    pass
+
 def binary_search_recursive(array, target, low, high):
+    if low > high:
+        raise TargetNotFoundException("Target not found in the list")
+    if not isinstance(array, list):
+        raise TypeError('Only list')
     if low > high:
         return -1
     mid = (low + high) // 2
